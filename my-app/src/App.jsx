68c7cf0de86992f68da3a0b9ed2e0722c6222b2a
Justin <jtrankings@gmail.com>
2025-05-24 13:51:23 -0400

    Add starting styling and information

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,14 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+      {/* <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -27,7 +27,27 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
+      <div className="top">
+        <div className="header">
+          <header><h2>Justin Tran Portfolio</h2></header>
+
+        </div>
+        <div className="body">
+          <div className="bodyTitle">
+            <div className="bodyTitleLeft">
+
+              <h1>Welcome to my personal website!</h1>
+            </div>
+            <img src='./public/portfolio_picture.JPG' height={200} width={500} style={{ borderRadius: 2000 }}></img>
+          </div>
+          <div className="bodyContent">
+            <h3>My name is Justin Tran. I recently graduated from the University of Cincinnati with a Bachelor of Science in Computer Science.
+              My skillset includes Frontend, Backend, and Full Stack capabilities, but my passion is in Web Development!
+            </h3>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
